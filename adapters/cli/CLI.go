bea73a20062a3c0e6c23322e51aa9eb12135297a
bryacka <bryacka@gmail.com>
2026-01-30 11:09:40 +0300

    - add edge cases to `TestCLI` - add checks to `extractSubjectAndHours` - transform `TestCLI` to table tests - add Sentinel Errors

--- adapters/cli/CLI.go
+++ adapters/cli/CLI.go
@@ -2,11 +2,18 @@ package cli
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"io"
 	"strconv"
 	"strings"
 
 	"github.com/bryack/study_hours_tracker/store"
+)
+
+var (
+	ErrNotEnoughArgs = errors.New("should be 2 arguments")
+	ErrInvalidHours  = errors.New("failed to parse hours")
 )
 
 type CLI struct {
@@ -21,13 +28,24 @@ func NewCLI(store store.SubjectStore, in io.Reader) *CLI {
 	}
 }
 
-func (cli *CLI) Run() {
+func (cli *CLI) Run() error {
 	cli.in.Scan()
-	cli.store.RecordHour(extractSubjectAndHours(cli.in.Text()))
+	s, h, err := extractSubjectAndHours(cli.in.Text())
+	if err != nil {
+		return fmt.Errorf("failed to extract subject and hours: %w", err)
+	}
+	cli.store.RecordHour(s, h)
+	return nil
 }
 
-func extractSubjectAndHours(userInput string) (string, int) {
+func extractSubjectAndHours(userInput string) (string, int, error) {
 	str := strings.Split(userInput, " ")
-	h, _ := strconv.Atoi(str[1])
-	return str[0], h
+	if len(str) < 2 {
+		return "", 0, fmt.Errorf("failed to parse: %w, got: %d", ErrNotEnoughArgs, len(str))
+	}
+	h, err := strconv.Atoi(str[1])
+	if err != nil {
+		return "", 0, fmt.Errorf("%w %v: %v", ErrInvalidHours, str[1], err)
+	}
+	return str[0], h, nil
 }
--- adapters/cli/CLI_test.go
+++ adapters/cli/CLI_test.go
@@ -10,38 +10,69 @@ import (
 )
 
 func TestCLI(t *testing.T) {
+	tests := []struct {
+		name            string
+		input           string
+		expectedSubject string
+		expectedHours   int
+		expectedErr     error
+	}{
+		{
+			name:            "record 'cli' hours",
+			input:           "cli 3",
+			expectedSubject: "cli",
+			expectedHours:   3,
+			expectedErr:     nil,
+		},
+		{
+			name:            "record 'bash' hours",
+			input:           "bash 5",
+			expectedSubject: "bash",
+			expectedHours:   5,
+			expectedErr:     nil,
+		},
+		{
+			name:            "handle parsing errors",
+			input:           "bufio five",
+			expectedSubject: "",
+			expectedHours:   0,
+			expectedErr:     cli.ErrInvalidHours,
+		},
+		{
+			name:            "not enough arguments",
+			input:           "bufio",
+			expectedSubject: "",
+			expectedHours:   0,
+			expectedErr:     cli.ErrNotEnoughArgs,
+		},
+	}
 
-	t.Run("record 'cli' hours", func(t *testing.T) {
-		in := strings.NewReader("cli 3")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			in := strings.NewReader(tt.input)
 
-		store := &testhelpers.StubSubjectStore{
-			Hours: map[string]int{},
-		}
-		trackerCLI := cli.NewCLI(store, in)
-		trackerCLI.Run()
+			store := &testhelpers.StubSubjectStore{
+				Hours: map[string]int{},
+			}
+			trackerCLI := cli.NewCLI(store, in)
+			err := trackerCLI.Run()
 
-		assertRecordTracker(t, store, "cli", 3)
-	})
-
-	t.Run("record 'bash' hours", func(t *testing.T) {
-		in := strings.NewReader("bash 5")
-
-		store := &testhelpers.StubSubjectStore{
-			Hours: map[string]int{},
-		}
-		trackerCLI := cli.NewCLI(store, in)
-		trackerCLI.Run()
-
-		assertRecordTracker(t, store, "bash", 5)
-	})
+			assert.ErrorIs(t, err, tt.expectedErr)
+			assertRecordTracker(t, store, tt.expectedSubject, tt.expectedHours, tt.expectedErr)
+		})
+	}
 }
 
-func assertRecordTracker(t testing.TB, store *testhelpers.StubSubjectStore, subject string, hours int) {
+func assertRecordTracker(t testing.TB, store *testhelpers.StubSubjectStore, subject string, hours int, err error) {
 	t.Helper()
 
-	assert.True(t, len(store.RecordCall) > 0)
+	if err == nil {
+		assert.True(t, len(store.RecordCall) > 0)
 
-	v, ok := store.Hours[subject]
-	assert.True(t, ok)
-	assert.Equal(t, hours, v)
+		v, ok := store.Hours[subject]
+		assert.True(t, ok)
+		assert.Equal(t, hours, v)
+	} else {
+		assert.Equal(t, 0, len(store.RecordCall))
+	}
 }
